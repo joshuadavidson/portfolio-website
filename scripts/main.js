be13e03e77005581641e58120aef773adec0b91e
Joshua Davidson <joshuadavidson@users.noreply.github.com>
2016-06-02 01:38:42 -0500

    Major Redesign. Pending review of skills.

---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+  //enable fullpage.js
+  $('#fullpage').fullpage({
+    anchors: ['intro', 'about', 'skills', 'portfolio', 'contact'],
+    scrollOverflow: true
+  });
+});
